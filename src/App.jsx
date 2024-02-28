8bd2afe4e20fb811cfb0087206bdd8a43dd93e6d
nayan-28 <nayanmalakar720@gmail.com>
2024-02-28 23:46:31 +0600

    Initially connected to the server side

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ChatComponent from "./ChatComponent";
+
+function App() {
+  return (
+    <>
+      <ChatComponent></ChatComponent>
+    </>
+  );
+}
+
+export default App;
